25bfa770c6d84cf68f616dc041b83fb2ee517e0d
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2023-10-26 09:10:48 +0200

    Add user input for columns and rows

--- C/2023/KW43/HTML-Code in C generieren/main.c
+++ C/2023/KW43/HTML-Code in C generieren/main.c
@@ -6,14 +6,24 @@ int main()
 
     FILE *fptr;
 
-    fptr = fopen("iindex.html", "w");
- 
-       if (fptr == NULL) {
-           printf("The file is not opened. The program will "
-                  "now exit.");
-         exit(0);
-       }
-   
+    fptr = fopen("C.html", "w");
+
+    if (fptr == NULL)
+    {
+        printf("The file is not opened. The program will "
+               "now exit.");
+        exit(0);
+    }
+
+    int rows = 0;
+    int columns = 0;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &columns);
+
     fprintf(fptr, "<!DOCTYPE html>\n");
     fprintf(fptr, "<head>\n");
     fprintf(fptr, "<title>Document</title>\n");
@@ -33,46 +43,20 @@ int main()
 
     fprintf(fptr, "<table>\n");
 
-    fprintf(fptr, "<tr>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<tr>\n");
-
-    fprintf(fptr, "<tr>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<tr>\n");
-
-    fprintf(fptr, "<tr>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<tr>\n");
-
-    fprintf(fptr, "<tr>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<td>Sample Text</td>\n");
-    fprintf(fptr, "<tr>\n");
+    for (int i = 0; i < rows; i++) {
+        fprintf(fptr, "<tr>\n");
+        for (int j = 0; j < columns; j++) {
+            fprintf(fptr, "<td>Sample Text</td>\n");
+        }
+        fprintf(fptr, "</tr>\n");
+    }
 
     fprintf(fptr, "<table>\n");
 
     fprintf(fptr, "</body>\n");
     fprintf(fptr, "</html>\n");
+
+    fclose(fptr);
 
     return 0;
 }
