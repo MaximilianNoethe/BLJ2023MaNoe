5f7895b2f33a212afd4cff16c7c0d02352b9a35a
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2023-11-03 12:05:50 +0100

    Add Dynamische Speicherallokierung to input

---
+++ C/2023/KW44/Input/Dynamische Speicherallokierung - Best practice/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int *nums = malloc(5 * sizeof(int));
+
+    if (!nums)
+        exit(1);
+
+    for (int i = 0; i < 5; i++)
+    {
+        nums[i] = i;
+
+        //*(nums + i) = i;
+    }
+
+    if (nums)
+    {
+        free(nums);
+        nums = NULL;
+    }
+
+    return 0;
+}
