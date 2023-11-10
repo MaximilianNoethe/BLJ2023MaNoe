6f7dd7de246cc496cbdcccf0f0673fb754ea626e
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2023-11-10 16:37:22 +0100

    Add changes

---
+++ C/2023/KW45/Slide Puzzle/main copy.c
@@ -0,0 +1,15 @@
+
+/**
+ * This programm depicts an all-time classic, the sliding puzzle. The one and
+ * only 0 represents the empty slot in the sliding puzzle. Moves (sliding up,
+ * down, left, right) are always made relative to the empty slot e.g. in the
+ * case of the initial state of the field, only the moves left (swap 0 with 15)
+ * and up (swap 0 with 8) can be made. This programm doesn't detect whether the
+ * field is sorted. It runs as long as the user doesn't enter a 0.
+ *
+ * @return 0 if the programm was successfully run.
+ */
+
+// TODO: Implement the rules for the sliding puzzle. In other words,
+// swap neighboured values (horizontally or vertically) in the 2D array
+// based on user input, but only if the move is legal (bounds checking).
