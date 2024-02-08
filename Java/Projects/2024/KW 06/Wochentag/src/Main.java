a3394a855e44cf52ea8856472a9988cc934bb11f
MaximilianNoethe <142780453+Maximilian-Noethe@users.noreply.github.com>
2024-02-08 11:44:45 +0100

    Add changes

--- Java/Projects/2024/KW 06/Wochentag/src/Main.java
+++ Java/Projects/2024/KW 06/Wochentag/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Monat> months
+
+        System.out.println("Enter your day: ");
+        int day = input.nextInt();
+
+        System.out.println("Enter your month: ");
+        int month = input.nextInt();
+
+        System.out.println("Enter your year: ");
+        int year = input.nextInt();
+
+
+
+
     }
 }
