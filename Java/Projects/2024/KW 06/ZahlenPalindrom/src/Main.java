b1b4800704e49df0f6fbf09e03e2b7cd2117c4bf
MaximilianNoethe <142780453+Maximilian-Noethe@users.noreply.github.com>
2024-02-15 09:26:46 +0100

    Complete change of the "ZahlenPalindrom" task

--- Java/Projects/2024/KW 06/ZahlenPalindrom/src/Main.java
+++ Java/Projects/2024/KW 06/ZahlenPalindrom/src/Main.java
@@ -3,17 +3,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ArrayList<Integer> digit = new ArrayList<>();
-
-        System.out.println("Enter your number to test if its a palindrome: ");
-        String num = input.next();
+        Scanner scanner = new Scanner(System.in);
 
 
+        System.out.println("Enter your number: ");
+        int zahl = scanner.nextInt();
 
 
+        if (istPalindrom(zahl)) {
+            System.out.println(zahl + " is a palindrom.");
+        } else {
+            System.out.println(zahl + " is not a palindrom.");
+        }
 
 
+    }
 
+
+    // Method to check for a palindrom
+    private static boolean istPalindrom(int zahl) {
+        int originalZahl = zahl;
+        int umgekehrteZahl = 0;
+
+        while (zahl > 0) {
+            int letzteZiffer = zahl % 10;
+            umgekehrteZahl = umgekehrteZahl * 10 + letzteZiffer;
+            zahl /= 10;
+        }
+
+        return originalZahl == umgekehrteZahl;
     }
 }
