ea2b39b3a2002a8bd69e8e6dc75bced626689f34
MaximilianNoethe <142780453+Maximilian-Noethe@users.noreply.github.com>
2024-02-21 20:52:20 +0100

    Add return value and add changes to grammar and method names

--- Java/Projects/2024/KW 08/Worlde/src/Word.java
+++ Java/Projects/2024/KW 08/Worlde/src/Word.java
@@ -5,6 +5,7 @@ import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.Random;
 
 public class Word {
+
     ArrayList<String> wordlist = new ArrayList<>();
     Random random = new Random();
 
@@ -18,24 +19,29 @@ public class Word {
             while (s.hasNextLine()) {
                 wordlist.add(s.nextLine());
             }
-            //System.out.println(wordlist);
+
+
         } catch (FileNotFoundException e) {
-            System.out.println("An error occured.");
+            System.out.println("An error occurred.");
         }
     }
 
 
-    public String randomWord() {
+    public String getRandomWord() {
+
         if (wordlist.isEmpty()) {
             System.out.println("No words are available.");
+            return "";
         } else {
             String randomWord = wordlist.get(random.nextInt(wordlist.size())).toLowerCase();
             // randomWord gets a value from the arraylist (wordList) -> gets the next index (random) -> with a range (size of the array list (wordList))
             // Every word will turn into lower case
 
             System.out.println(randomWord);
+            return randomWord;
         }
-        return null;
+
     }
+
 
 }
