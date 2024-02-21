3c3dbc871c433457d5b3ceca8bcca69651450aa0
MaximilianNoethe <142780453+Maximilian-Noethe@users.noreply.github.com>
2024-02-21 13:53:21 +0100

    implement the readFile, randomWord and startGame method

--- Java/Projects/2024/KW 08/Worlde/src/Game.java
+++ Java/Projects/2024/KW 08/Worlde/src/Game.java
@@ -1,2 +1,10 @@
 public class Game {
+    Word word = new Word();
+
+    public void startGame(){
+        word.readFile("src\\wordlist.txt");
+        word.randomWord();
+    }
+
+
 }
---
+++ Java/Projects/2024/KW 08/Worlde/src/Word.java
@@ -0,0 +1,41 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Random;
+
+public class Word {
+    ArrayList<String> wordlist = new ArrayList<>();
+    Random random = new Random();
+
+    public void readFile(String fileName) {
+
+        try {
+            File myFile = new File(fileName);
+            Scanner s = new Scanner(myFile);
+
+
+            while (s.hasNextLine()) {
+                wordlist.add(s.nextLine());
+            }
+            //System.out.println(wordlist);
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured.");
+        }
+    }
+
+
+    public String randomWord() {
+        if (wordlist.isEmpty()) {
+            System.out.println("No words are available.");
+        } else {
+            String randomWord = wordlist.get(random.nextInt(wordlist.size())).toLowerCase();
+            // randomWord gets a value from the arraylist (wordList) -> gets the next index (random) -> with a range (size of the array list (wordList))
+            // Every word will turn into lower case
+
+            System.out.println(randomWord);
+        }
+        return null;
+    }
+
+}
