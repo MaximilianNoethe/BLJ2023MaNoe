18809ba0cac2210f5e4b925f5f6f8996f94459df
MaximilianNoethe <142780453+Maximilian-Noethe@users.noreply.github.com>
2024-02-21 20:53:16 +0100

    Delete user input and insert game.userWord method from class "Game"

--- Java/Projects/2024/KW 08/Worlde/src/Main.java
+++ Java/Projects/2024/KW 08/Worlde/src/Main.java
@@ -5,15 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Game game = new Game();
-        Word word = new Word();
 
         game.startGame();
-        System.out.println("Enter your word: ");
-        String wordInput = input.next();
 
-        if (wordInput.equals(word.randomWord())){
-            System.out.println("Amazing! You Win!");
-        }
+        game.userWord();
 
     }
 }
