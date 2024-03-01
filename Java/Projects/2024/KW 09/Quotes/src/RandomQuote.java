18025cbda6d176ee00eb49f174fa632cef161e18
MaximilianNoethe <142780453+Maximilian-Noethe@users.noreply.github.com>
2024-03-01 09:27:43 +0100

    Add test file

--- Java/Projects/2024/KW 09/Quotes/src/RandomQuote.java
+++ Java/Projects/2024/KW 09/Quotes/src/RandomQuote.java
@@ -1,2 +1,44 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class RandomQuote {
+
+
+
+
+    private void buildRequest() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://zenquotes.io/api/random"))
+                    .GET()
+                    .build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+
+    private void getResponse() {
+        try {
+            HttpRequest request = null;
+            HttpResponse<String> response = HttpClient.newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void printResponse(){
+        System.out.println();
+    }
+
 }
