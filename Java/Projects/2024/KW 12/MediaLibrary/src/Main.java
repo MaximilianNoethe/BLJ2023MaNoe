291ec74d656b98088df0385a032d4e12e8b3f2bf
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-03-20 16:19:48 +0100

    Change from 3 arraylists to one and update objects, switch cases and add while loop for the option to exit the media-library

--- Java/Projects/2024/KW 12/MediaLibrary/src/Main.java
+++ Java/Projects/2024/KW 12/MediaLibrary/src/Main.java
@@ -5,116 +5,147 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Book> books = new ArrayList<>();
-        ArrayList<Videogame> videogames = new ArrayList<>();
-        ArrayList<Song> songs = new ArrayList<>();
+
+        ArrayList<Media> medias = new ArrayList<>();
 
 
-        Book b1 = new Book("Heroes of Olympus: The Blood of Olympus",
+        medias.add(new Book("Heroes of Olympus: The Blood of Olympus",
                 "English",
                 "Fantasy, Young Adult",
                 14.99,
                 "Rick Riordan",
-                528);
+                528));
 
-        Book b2 = new Book("Bleach volume 1",
+        medias.add(new Book("Bleach volume 1",
                 "English",
                 "Shōnen-Manga (Action, Adventure)",
                 12.00,
                 "Tite Kubo",
-                192);
+                192));
 
-        Book b3 = new Book("Vinland Saga volume 24",
+        medias.add(new Book("Vinland Saga volume 24",
                 "English",
                 "Seinen-Manga (Action, Adventure, Drama)",
                 11.00,
                 "Makoto Yukimura",
-                250);
+                250));
 
 
-        Videogame v1 = new Videogame("Elden Ring",
+        medias.add(new Videogame("Elden Ring",
                 "English",
                 "Action RPG, Souls-Like",
                 69.90,
                 "Banda Namco Entertainment",
                 "Console and PC",
-                75);
+                75));
 
-        Videogame v2 = new Videogame("Ghost of Tsushima",
+        medias.add(new Videogame("Ghost of Tsushima",
                 "English",
                 "Action-Adventure, Open World",
                 50.00,
                 "Sony",
                 "Console (PS-Only)",
-                45);
+                45));
 
-        Videogame v3 = new Videogame("God of War",
+        medias.add(new Videogame("God of War",
                 "English",
                 "Action-Adventure, Hack and Slash",
                 55.00,
                 "Sony",
                 "Console (PS-Only)",
-                50);
+                50));
 
 
-        Song s1 = new Song("Redrum",
+        medias.add(new Song("Redrum",
                 "English",
                 "Hip-Hop",
                 1.50,
                 "21 Savage",
-                2.43);
+                2.43));
 
-        Song s2 = new Song("Duvet",
+        medias.add(new Song("Duvet",
                 "English",
                 "Trip-hop",
                 1.29,
                 "Boa",
-                3.55);
+                3.55));
 
-        Song s3 = new Song("679",
+        medias.add(new Song("679",
                 "English",
                 "Hip-Hop",
                 1.29,
                 "Fetty Wap",
-                3.09);
+                3.09));
+
+        Boolean exit = false;
+        while (!exit) {
+            System.out.println("""
+                    Welcome to the coolest media library
+                                    
+                    What kind of media do you want to see?
+                                    
+                    --Media--
+                    Books (b)
+                    Videogames (v)
+                    Songs (s)
+                    Everything (a)
+                    
+                    Exit (x)
+                                    
+                    Please enter your choice:""");
+            String input = scanner.next();
+
+            switch (input) {
+                case "b":
+                    System.out.println("Books:");
+                    for (Media media : medias) {
+                        if (media instanceof Book) {
+                            media.printInfo();
+                        }
+
+                    }
+                    break;
 
 
-        System.out.println("""
-                Welcome to the coolest media library
-                                
-                What kind of media do you want to see?
-                                
-                --Media--
-                Books (b)
-                Videogames (v)
-                Songs (s)
-                                
-                Please enter your choice:
-                """);
-        String input = scanner.next();
+                case "v":
+                    System.out.println("Videogames:");
+                    for (Media media : medias) {
+                        if (media instanceof Videogame) {
+                            media.printInfo();
+                        }
 
-        switch (input) {
-            case "b":
-                b1.printInfo();
-                b2.printInfo();
-                b3.printInfo();
-                break;
+                    }
+                    break;
 
 
-            case "v":
-                v1.printInfo();
-                v2.printInfo();
-                v3.printInfo();
-                break;
+                case "s":
+                    System.out.println("Songs:");
+                    for (Media media : medias) {
+                        if (media instanceof Song) {
+                            media.printInfo();
+                        }
+
+                    }
+                    break;
+
+                case "a":
+                    System.out.println("Everything:");
+                    for (Media media : medias) {
+                        media.printInfo();
+                    }
+                    break;
+
+                case "x":
+                    exit = true;
+                    System.out.println("Thank you for visiting my Media-library, see you next time!");
+                    break;
+
+                default:
+                    System.out.println("Invalid input");
+                    break;
 
 
-            case "s1":
-                s1.printInfo();
-                s2.printInfo();
-                s3.printInfo();
-                break;
-
-
+            }
         }
     }
 }
