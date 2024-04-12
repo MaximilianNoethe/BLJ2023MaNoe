51986b08170573b6891db011f9ac2e7395c00a09
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-12 12:09:07 +0200

    Add Navbar to HomePage

---
+++ TypeScript/Sura-Generator/src/Components/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Grid, TextField } from "@mui/material";
+import Navbar from "../Molecule/Navbar";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      
+    
+    </>
+  );
+}
+
+export default HomePage;
