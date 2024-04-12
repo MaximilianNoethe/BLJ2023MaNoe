f1699f352a599330661dd1db762fd005b7a8a672
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-12 12:09:40 +0200

    Add new route

--- TypeScript/Sura-Generator/src/App.tsx
+++ TypeScript/Sura-Generator/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ShowAyah from "./Components/Pages/ShowAyah";
+import HomePage from "./Components/Pages/HomePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="ayah" Component={HomePage} />
           <Route path="ayah/:ayahId/en.asad" Component={ShowAyah} />
         </Routes>
       </BrowserRouter>
