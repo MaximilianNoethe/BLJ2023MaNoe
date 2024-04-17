cb2c33f432d8c78553c7beeff224636e23b572e6
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-17 16:23:17 +0200

    Create starter and add data for lines and points

--- Java/Projects/2024/KW 16/CoordinateSystem/src/Starter.java
+++ Java/Projects/2024/KW 16/CoordinateSystem/src/Starter.java
@@ -1,5 +1,14 @@
 public class Starter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        CoordinateSystem cs = new CoordinateSystem(500);
+
+        cs.addPoint(new CSPoint(30, 60));
+
+        cs.addLine(new CSLineSegment(new CSPoint(34, 62), new CSPoint(-34, 22)));
+
+
+        new CSRenderer(cs, 1, 3);
     }
 }
