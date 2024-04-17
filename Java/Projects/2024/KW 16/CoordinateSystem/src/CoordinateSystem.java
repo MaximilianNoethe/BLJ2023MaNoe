c75cb429e4dc15c1827a88cf827f26b09e282066
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-17 16:21:53 +0200

    Create CoordinateSystem and add logic for both lines and points

--- Java/Projects/2024/KW 16/CoordinateSystem/src/CoordinateSystem.java
+++ Java/Projects/2024/KW 16/CoordinateSystem/src/CoordinateSystem.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoordinateSystem {
+
+    private List<CSPoint> points = new ArrayList<CSPoint>();
+
+    private List<CSLineSegment> lines = new ArrayList<CSLineSegment>();
+
+    private int size;
+
+
+    public void addLine(CSLineSegment line) {
+        lines.add(line);
+    }
+
+    public List<CSLineSegment> getAllLines() {
+        return lines;
+    }
+
+
+    public CoordinateSystem(int size) {
+        if (size <= 0 || size % 20 != 0) {
+            throw new IllegalArgumentException("Your number needs to be greater than 0 and it must be possible to divide it by 20.");
+        }
+
+        this.size = size;
+    }
+
+
+    public void addPoint(CSPoint point) {
+        points.add(point);
+    }
+
+    public List<CSPoint> getAllPoints() {
+        return points;
+    }
+
+
+    public int getCoordinateSystemSize() {
+        return size;
+    }
+
+
 }
