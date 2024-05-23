e96a91e8c9d2c6ef9524ba8b39bf736d1503905d
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-23 11:01:13 +0200

    Add constructor and getters and setters

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/Person.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/Person.java
@@ -2,5 +2,36 @@ import java.time.LocalDate;
 
 public class Person {
     private LocalDate birthYear;
+    private int age;
+    private String name;
 
+    public Person(LocalDate birthYear, int age, String name) {
+        this.birthYear = birthYear;
+        this.age = age;
+        this.name = name;
+    }
+
+    public LocalDate getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(LocalDate birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
