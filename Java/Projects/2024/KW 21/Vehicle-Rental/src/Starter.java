6a84fd24226f55860dda4000edfa6f401dcf4e83
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-23 14:55:44 +0200

    Add data for the contract in starter class

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/Starter.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/Starter.java
@@ -1,7 +1,29 @@
+import exceptions.DenyListedPersonException;
+import exceptions.MinorAgeException;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Starter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MinorAgeException, DenyListedPersonException {
+        VehicleRentalManager vrm = new VehicleRentalManager();
+
+        Person testPerson = new Person(LocalDate.of(2007, 1, 16),"Thorfinn");
+        vrm.addPersonToCustomerList(testPerson);
+
+        Car testCar = new Car(560, 250.50, "Internal combustion engine", "Mercedes", 4, 4);
+        vrm.addVehicle(testCar);
+
+        LocalDate startDate = LocalDate.of(2024, 05, 23);
+        LocalDate endDate = LocalDate.of(2024, 06, 23);
+        String contractModalities = "content";
+
+        Contract contract = new Contract(startDate, endDate, contractModalities, testPerson, testCar);
+
+
+
+
+        vrm.createContract(contract);
 
     }
 }
