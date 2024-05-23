68eedcc83753e42f324398028facb21a14556233
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-23 11:06:51 +0200

    Add constructor and methods to create a contract, add a person and vehicle and add a person into the denylist

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/VehicleRentalManager.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/VehicleRentalManager.java
@@ -1,3 +1,7 @@
+import exceptions.DenyListedPersonException;
+import exceptions.MinorAgeException;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class VehicleRentalManager {
@@ -5,4 +9,44 @@ public class VehicleRentalManager {
     private ArrayList<Person> denyList;
     private ArrayList<Vehicle> vehicles;
     private ArrayList<Contract> contracts;
+
+
+    public VehicleRentalManager() {
+        this.customerList = new ArrayList<Person>();
+        this.denyList = new ArrayList<Person>();
+        this.vehicles = new ArrayList<Vehicle>();
+        this.contracts = new ArrayList<Contract>();
+    }
+
+    private int MINIMUM_AGE = 22;
+
+    private void createContract(Contract contract) throws DenyListedPersonException, MinorAgeException {
+        Person person = contract.getPerson();
+        if (denyList.contains(person)) {
+            throw new DenyListedPersonException();
+        } else if (person.getAge() < MINIMUM_AGE) {
+            throw new MinorAgeException();
+        } else {
+            contracts.add(contract);
+        }
+    }
+
+
+    public void addPersonToCustomerList(Person person) {
+        customerList.add(person);
+    }
+
+    public void addPersonToDenyList(Person person) {
+        denyList.add(person);
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+
+    public void addContract(Contract contract) {
+        contracts.add(contract);
+    }
+
+
 }
