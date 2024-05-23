5c7fd1ecae6168a1c7942a2731a5eb43c3fa409a
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-23 11:00:12 +0200

    Add constructor to Car

---
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/Car.java
@@ -0,0 +1,6 @@
+public class Car extends LandVehicle{
+
+    public Car(int weight, double pricePerHour, String motorType, String vehicleBrand, int seatNumber, int wheelCount) {
+        super(weight, pricePerHour, motorType, vehicleBrand, seatNumber, wheelCount);
+    }
+}
