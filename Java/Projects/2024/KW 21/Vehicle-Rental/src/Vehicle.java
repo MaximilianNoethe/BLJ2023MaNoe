fe7556c83f90a4abe60c41a7b06670baea53afac
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-23 11:02:28 +0200

    Add fields and constructor

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/Vehicle.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/Vehicle.java
@@ -1,2 +1,15 @@
 public class Vehicle {
+    private int weight;
+    private double pricePerHour;
+    private String motorType;
+    private String vehicleBrand;
+    private int seatNumber;
+
+    public Vehicle(int weight, double pricePerHour, String motorType, String vehicleBrand, int seatNumber) {
+        this.weight = weight;
+        this.pricePerHour = pricePerHour;
+        this.motorType = motorType;
+        this.vehicleBrand = vehicleBrand;
+        this.seatNumber = seatNumber;
+    }
 }
