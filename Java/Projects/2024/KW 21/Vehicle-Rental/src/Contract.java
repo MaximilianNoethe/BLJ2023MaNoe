39e830577379b51336b5ea663e852dcc85ca65a8
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-23 11:00:50 +0200

    Add new fields and constructor aswell as getters

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/Contract.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/Contract.java
@@ -1,5 +1,37 @@
 import java.time.LocalDate;
 
 public class Contract {
-    LocalDate rentDuration;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String contractModalities;
+    private Person person;
+    private Vehicle vehicle;
+
+    public Contract(LocalDate startDate, LocalDate endDate, String contractModalities, Person person, Vehicle vehicle) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.contractModalities = contractModalities;
+        this.person = person;
+        this.vehicle = vehicle;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public String getContractModalities() {
+        return contractModalities;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
 }
