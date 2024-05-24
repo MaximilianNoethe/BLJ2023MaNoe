d15daa481cc0c9ec480fd12f5a7de36aa6bb80c8
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-24 09:21:22 +0200

    Update errormessage

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/exceptions/LeaseLengthCollisionException.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/exceptions/LeaseLengthCollisionException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class LeaseLengthCollisionException extends ContractException{
     public LeaseLengthCollisionException(){
-        super("ERROR");
+        super("I'm sorry but there is someone else renting this car during your rental period already.");
     }
 }
