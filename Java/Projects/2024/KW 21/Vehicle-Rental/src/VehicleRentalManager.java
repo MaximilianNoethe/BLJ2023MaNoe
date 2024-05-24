55a38178427ad1e940b93a4e1b61dfb722d93674
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-24 09:23:01 +0200

    Update createContract method and format code

--- Java/Projects/2024/KW 21/Vehicle-Rental/src/VehicleRentalManager.java
+++ Java/Projects/2024/KW 21/Vehicle-Rental/src/VehicleRentalManager.java
@@ -19,31 +19,7 @@ public class VehicleRentalManager {
         this.contracts = new ArrayList<Contract>();
     }
 
-    private final int MINIMUM_AGE = 22;
-
-    public void createContract(Contract contract) throws DenyListedPersonException, MinorAgeException, LeaseLengthCollisionException {
-        Person person = contract.getPerson();
-        if (denyList.contains(person)) {
-            throw new DenyListedPersonException();
-        } else if (person.getAge() < MINIMUM_AGE) {
-            throw new MinorAgeException();
-        } else {
-            checkCollision(contract.getVehicle(), contract.getStartDate(), contract.getEndDate());
-            contracts.add(contract);
-            System.out.println("Thank you for signing the contract!");
-        }
-    }
-
-    private void checkCollision(Vehicle vehicle, LocalDate startDate, LocalDate endDate) throws LeaseLengthCollisionException {
-        for (Contract validContracts : contracts) {
-            if (validContracts.getVehicle().equals(vehicle)) {
-                if (startDate.isAfter(validContracts.getStartDate()) && startDate.isBefore(validContracts.getEndDate()) || endDate.isAfter(validContracts.getStartDate()) && endDate.isBefore(validContracts.getEndDate()) || startDate.equals(validContracts.getStartDate()) || endDate.equals(validContracts.getEndDate())) {
-                    throw new LeaseLengthCollisionException();
-                }
-            }
-        }
-    }
-
+    private final int MINIMUM_AGE = 12;
 
     public void addPersonToCustomerList(Person person) {
         customerList.add(person);
@@ -57,5 +33,29 @@ public class VehicleRentalManager {
         vehicles.add(vehicle);
     }
 
+    public void createContract(Contract contract) throws DenyListedPersonException, MinorAgeException, LeaseLengthCollisionException {
+        Person person = contract.getPerson();
+        if (denyList.contains(person)) {
+            throw new DenyListedPersonException();
+        } else if (person.getAge() < MINIMUM_AGE) {
+            throw new MinorAgeException();
+        } else if (checkCollision(contract.getVehicle(), contract.getStartDate(), contract.getEndDate())) {
+            throw new LeaseLengthCollisionException();
+        } else {
+            contracts.add(contract);
+            System.out.println("Thank you for signing the contract!");
+        }
+    }
+
+    private boolean checkCollision(Vehicle vehicle, LocalDate startDate, LocalDate endDate) throws LeaseLengthCollisionException {
+        for (Contract validContracts : contracts) {
+            if (validContracts.getVehicle().equals(vehicle)) {
+                if (startDate.isAfter(validContracts.getStartDate()) && startDate.isBefore(validContracts.getEndDate()) || endDate.isAfter(validContracts.getStartDate()) && endDate.isBefore(validContracts.getEndDate()) || startDate.equals(validContracts.getStartDate()) || endDate.equals(validContracts.getEndDate())) {
+                    throw new LeaseLengthCollisionException();
+                }
+            }
+        }
+        return true;
+    }
 
 }
