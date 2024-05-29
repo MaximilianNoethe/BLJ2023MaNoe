d4a05c1e40e2681cd21b8f7191681bcb0d3f86b8
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-29 10:00:39 +0200

    Update ImageHandler class with method to noise an image

--- Java/Projects/2024/KW 21/ImageChallenge/src/ImageHandler.java
+++ Java/Projects/2024/KW 21/ImageChallenge/src/ImageHandler.java
@@ -4,12 +4,27 @@ import java.io.File;
 import java.io.IOException;
 
 public class ImageHandler {
+    Noise n = new Noise();
+    private double noisePercentage = 0.25;
     private String inputPath = "C:\\Users\\sussy\\Downloads\\Aryan_Bisen.jpg";
     private String outputPath = "C:\\Users\\sussy\\Downloads\\Aryan_Bisen_new.png";
 
     public void readAndWriteImage() throws IOException {
         File file = new File(inputPath);
         BufferedImage image = ImageIO.read(file);
-        ImageIO.write(image, "png",new File(outputPath));
+        ImageIO.write(image, "png", new File(outputPath));
     }
+
+    public void noiseImage() {
+        try {
+            BufferedImage image = ImageIO.read(new File(inputPath));
+            BufferedImage noisedImage = n.addNoise(image, noisePercentage);
+
+            ImageIO.write(noisedImage, "png", new File(outputPath));
+            System.out.println();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
