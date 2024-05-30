ad54e0959e4c75b985716f33fc471432237c9535
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-05-30 16:49:57 +0200

    Update Main class

--- Java/Projects/2024/KW 22/ImageChallenge2/src/Main.java
+++ Java/Projects/2024/KW 22/ImageChallenge2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String inputPath = "C:\\Users\\sussy\\Downloads\\Aryan_Bisen.jpg";
+        ImageHandler handler = new ImageHandler();
+
+        handler.colorToGray(inputPath);
+        handler.convertToAscii("C:\\Users\\sussy\\Downloads\\Aryan_Bisen_Gray.jpg", handler.DEFAULT_ASCII_CHARS, "C:\\Users\\sussy\\Downloads\\Aryan_Bisen_Gray.txt");
     }
 }
