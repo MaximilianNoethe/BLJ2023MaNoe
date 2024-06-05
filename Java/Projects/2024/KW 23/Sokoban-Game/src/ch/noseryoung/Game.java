beca0b39187ca17708fa4784ef5c04a040d0b496
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-05 16:17:09 +0200

    Add logic to move within the border and a reset function

--- Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Game.java
+++ Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Game.java
@@ -3,7 +3,7 @@ package ch.noseryoung;
 import java.awt.*;
 
 public class Game {
-    private Point p = new Point();
+    private Point currentPos = new Point();
     private int[][] gameField;
 
     public Game() {
@@ -24,8 +24,8 @@ public class Game {
         for (int i = 0; i < getColCount(); i++) {
             for (int j = 0; j < getRowCount(); j++) {
                 if (gameField[i][j] == 2) {
-                    p.x = i;
-                    p.y = j;
+                    currentPos.x = i;
+                    currentPos.y = j;
                 }
             }
         }
@@ -34,44 +34,79 @@ public class Game {
     public void moveUp() {
         System.out.println("UP");
 
-        gameField[p.x][p.y] = 0;
-        p.x -= 1;
-        gameField[p.x][p.y] = 2;
+        if (currentPos.y > 0 && gameField[currentPos.y - 1][currentPos.x] != 1) {
+            gameField[currentPos.y][currentPos.x] = 0;
+            currentPos.y -= 1;
+            gameField[currentPos.y][currentPos.x] = 2;
+        } else {
+
+            System.out.println("You can't pass the wall!!");
+        }
     }
 
     public void moveDown() {
         System.out.println("DOWN");
 
-        gameField[p.x][p.y] = 0;
-        p.x += 1;
-        gameField[p.x][p.y] = 2;
+        if (currentPos.y < getColCount() - 1 && gameField[currentPos.y + 1][currentPos.x] != 1) {
+            gameField[currentPos.y][currentPos.x] = 0;
+            currentPos.y += 1;
+            gameField[currentPos.y][currentPos.x] = 2;
+        } else {
+
+            System.out.println("You can't pass the wall!!");
+        }
     }
 
     public void moveLeft() {
         System.out.println("LEFT");
 
-       // if (p.x >= 0 && gameField[p.y][p.x + 1] != 1) {
-            gameField[p.x][p.y] = 0;
-            p.y -= 1;
-            gameField[p.x][p.y] = 2;
-       // } else {
+        if (currentPos.x > 0 && gameField[currentPos.y][currentPos.x - 1] != 1) {
+            gameField[currentPos.y][currentPos.x] = 0;
+            currentPos.x -= 1;
+            gameField[currentPos.y][currentPos.x] = 2;
+        } else {
 
-
+            System.out.println("You can't pass the wall!!");
         }
-
-
-   // }
+    }
 
     public void moveRight() {
         System.out.println("RIGHT");
 
-        gameField[p.x][p.y] = 0;
-        p.y += 1;
-        gameField[p.x][p.y] = 2;
+        if (currentPos.x < getRowCount() - 1 && gameField[currentPos.y][currentPos.x + 1] != 1) {
+            gameField[currentPos.y][currentPos.x] = 0;
+            currentPos.x += 1;
+            gameField[currentPos.y][currentPos.x] = 2;
+        } else {
+
+            System.out.println("You can't pass the wall!!");
+        }
     }
 
     public void resetField() {
         System.out.println("ESC");
+        gameField = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 0, 0, 0, 1, 0, 0},
+                {0, 1, 4, 2, 3, 0, 0, 1, 0, 0},
+                {0, 1, 1, 1, 0, 3, 4, 1, 0, 0},
+                {0, 1, 4, 1, 1, 3, 0, 1, 0, 0},
+                {0, 1, 0, 1, 0, 4, 0, 1, 1, 0},
+                {0, 1, 3, 0, 3, 3, 3, 4, 1, 0},
+                {0, 1, 0, 0, 0, 4, 0, 0, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        for (int i = 0; i < getColCount(); i++) {
+            for (int j = 0; j < getRowCount(); j++) {
+                if (gameField[i][j] == 2) {
+                    currentPos.x = i;
+                    currentPos.y = j;
+                }
+            }
+        }
     }
 
     public int[][] getField() {
