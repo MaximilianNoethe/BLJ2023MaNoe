dbded3f49001934debe4414f0963df636e76d8b1
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-05 14:08:14 +0200

    Create logic to move the player

--- Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Game.java
+++ Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Game.java
@@ -1,52 +1,88 @@
 package ch.noseryoung;
 
+import java.awt.*;
+
 public class Game {
+    private Point p = new Point();
+    private int[][] gameField;
 
-  private int[][] gameField;
+    public Game() {
+        gameField = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 0, 0, 0, 1, 0, 0},
+                {0, 1, 4, 2, 3, 0, 0, 1, 0, 0},
+                {0, 1, 1, 1, 0, 3, 4, 1, 0, 0},
+                {0, 1, 4, 1, 1, 3, 0, 1, 0, 0},
+                {0, 1, 0, 1, 0, 4, 0, 1, 1, 0},
+                {0, 1, 3, 0, 3, 3, 3, 4, 1, 0},
+                {0, 1, 0, 0, 0, 4, 0, 0, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
 
-  public Game() {
-    gameField = new int[][] { 
-      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-      { 0, 0, 0, 1, 1, 1, 1, 1, 0, 0 },
-      { 0, 1, 1, 1, 0, 0, 0, 1, 0, 0 }, 
-      { 0, 1, 3, 2, 3, 0, 0, 1, 0, 0 },
-      { 0, 1, 1, 1, 0, 3, 4, 1, 0, 0 }, 
-      { 0, 1, 4, 1, 1, 3, 0, 1, 0, 0 },
-      { 0, 1, 0, 1, 0, 4, 0, 1, 1, 0 }, 
-      { 0, 1, 3, 0, 3, 3, 3, 4, 1, 0 },
-      { 0, 1, 0, 0, 0, 4, 0, 0, 1, 0 }, 
-      { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-    };
-  }
-  
-  public void moveUp() {
-    System.out.println("UP");
-  }
+        for (int i = 0; i < getColCount(); i++) {
+            for (int j = 0; j < getRowCount(); j++) {
+                if (gameField[i][j] == 2) {
+                    p.x = i;
+                    p.y = j;
+                }
+            }
+        }
+    }
 
-  public void moveDown() {
-    System.out.println("DOWN");
-  }
+    public void moveUp() {
+        System.out.println("UP");
 
-  public void moveLeft() {
-    System.out.println("LEFT");
-  }
+        gameField[p.x][p.y] = 0;
+        p.x -= 1;
+        gameField[p.x][p.y] = 2;
+    }
 
-  public void moveRight() { System.out.println("RIGHT"); }
+    public void moveDown() {
+        System.out.println("DOWN");
 
-  public void resetField() {
-    System.out.println("ESC");
-  }
+        gameField[p.x][p.y] = 0;
+        p.x += 1;
+        gameField[p.x][p.y] = 2;
+    }
 
-  public int[][] getField() {
-    return gameField;
-  }
+    public void moveLeft() {
+        System.out.println("LEFT");
 
-  public int getColCount() {
-    return gameField.length;
-  }
+       // if (p.x >= 0 && gameField[p.y][p.x + 1] != 1) {
+            gameField[p.x][p.y] = 0;
+            p.y -= 1;
+            gameField[p.x][p.y] = 2;
+       // } else {
 
-  public int getRowCount() {
-    return gameField[0].length;
-  }
+
+        }
+
+
+   // }
+
+    public void moveRight() {
+        System.out.println("RIGHT");
+
+        gameField[p.x][p.y] = 0;
+        p.y += 1;
+        gameField[p.x][p.y] = 2;
+    }
+
+    public void resetField() {
+        System.out.println("ESC");
+    }
+
+    public int[][] getField() {
+        return gameField;
+    }
+
+    public int getColCount() {
+        return gameField.length;
+    }
+
+    public int getRowCount() {
+        return gameField[0].length;
+    }
 }
--- Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Starter.java
+++ Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Starter.java
@@ -2,6 +2,6 @@ package ch.noseryoung;
 
 public class Starter {
   public static void main(String[] args) {
-    new GameIO(new Game(), 32, 20);
+    new GameIO(new Game(), 64, 0.9);
   }
 }
