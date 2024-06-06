1d3988f9e3924c9aa88f5e6b3540799dc9bb1601
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-06 11:12:31 +0200

    Put the resetField into the Game class to avoid duplicates, checkCheckpoint added to keep the checkpoint color in the game

--- Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Game.java
+++ Java/Projects/2024/KW 23/Sokoban-Game/src/ch/noseryoung/Game.java
@@ -1,54 +1,32 @@
 package ch.noseryoung;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Game {
     private Point currentPos = new Point();
+    ArrayList<Point> checkPoints = new ArrayList<Point>();
     private int[][] gameField;
 
     public Game() {
-        gameField = new int[][]{
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
-                {0, 1, 1, 1, 0, 0, 0, 1, 0, 0},
-                {0, 1, 4, 2, 3, 0, 0, 1, 0, 0},
-                {0, 1, 1, 1, 0, 3, 4, 1, 0, 0},
-                {0, 1, 4, 1, 1, 3, 0, 1, 0, 0},
-                {0, 1, 0, 1, 0, 4, 0, 1, 1, 0},
-                {0, 1, 3, 0, 3, 3, 3, 4, 1, 0},
-                {0, 1, 0, 0, 0, 4, 0, 0, 1, 0},
-                {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        for (int i = 0; i < getColCount(); i++) {
-            for (int j = 0; j < getRowCount(); j++) {
-                if (gameField[i][j] == 2) {
-                    currentPos.x = i;
-                    currentPos.y = j;
-                }
-            }
-        }
+        resetField();
     }
 
     public void moveUp() {
         System.out.println("UP");
-
         if (currentPos.y > 0 && gameField[currentPos.y - 1][currentPos.x] != 1) {
-            gameField[currentPos.y][currentPos.x] = 0;
+            checkCheckpoints();
             currentPos.y -= 1;
             gameField[currentPos.y][currentPos.x] = 2;
         } else {
-
             System.out.println("You can't pass the wall!!");
         }
     }
 
     public void moveDown() {
         System.out.println("DOWN");
-
         if (currentPos.y < getColCount() - 1 && gameField[currentPos.y + 1][currentPos.x] != 1) {
-            gameField[currentPos.y][currentPos.x] = 0;
+            checkCheckpoints();
             currentPos.y += 1;
             gameField[currentPos.y][currentPos.x] = 2;
         } else {
@@ -58,26 +36,31 @@ public class Game {
 
     public void moveLeft() {
         System.out.println("LEFT");
-
         if (currentPos.x > 0 && gameField[currentPos.y][currentPos.x - 1] != 1) {
-            gameField[currentPos.y][currentPos.x] = 0;
+            checkCheckpoints();
             currentPos.x -= 1;
             gameField[currentPos.y][currentPos.x] = 2;
         } else {
-
             System.out.println("You can't pass the wall!!");
         }
     }
 
     public void moveRight() {
         System.out.println("RIGHT");
-
         if (currentPos.x < getRowCount() - 1 && gameField[currentPos.y][currentPos.x + 1] != 1) {
-            gameField[currentPos.y][currentPos.x] = 0;
+            checkCheckpoints();
             currentPos.x += 1;
             gameField[currentPos.y][currentPos.x] = 2;
         } else {
             System.out.println("You can't pass the wall!!");
+        }
+    }
+
+    public void checkCheckpoints() {
+        if (checkPoints.contains(new Point(currentPos.x, currentPos.y))) {
+            gameField[currentPos.y][currentPos.x] = 4;
+        } else {
+            gameField[currentPos.y][currentPos.x] = 0;
         }
     }
 
@@ -96,12 +79,15 @@ public class Game {
                 {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
+        checkPoints.clear();
 
-        for (int i = 0; i < getColCount(); i++) {
-            for (int j = 0; j < getRowCount(); j++) {
-                if (gameField[i][j] == 2) {
-                    currentPos.x = i;
-                    currentPos.y = j;
+        for (int y = 0; y < getColCount(); y++) {
+            for (int x = 0; x < getRowCount(); x++) {
+                if (gameField[y][x] == 2) {
+                    currentPos.x = x;
+                    currentPos.y = y;
+                } else if (gameField[y][x] == 4) {
+                    checkPoints.add(new Point(x, y));
                 }
             }
         }
@@ -118,4 +104,6 @@ public class Game {
     public int getRowCount() {
         return gameField[0].length;
     }
+
+
 }
