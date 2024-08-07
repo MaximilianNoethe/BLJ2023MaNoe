d1f313ecfdca2f1ab8c59c29e5e355243443d7b3
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-08-07 09:05:48 +0200

    Create Transaction class and implement variables to use them as parameters for each transaction

---
+++ Java/Projects/2024/KW 32/Financial Tracker/src/Transaction.java
@@ -0,0 +1,17 @@
+public class Transaction {
+    private String name;
+    private double value;
+
+    public Transaction(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
