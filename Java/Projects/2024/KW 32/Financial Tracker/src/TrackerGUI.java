3c74b50a94d4c153c5567ccc9f9598b7c0df27c3
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-08-07 14:37:13 +0200

    Update income, expense and balance displays. Implement ActionListener for buttons to add incomes and expenses aswell as their names

--- Java/Projects/2024/KW 32/Financial Tracker/src/TrackerGUI.java
+++ Java/Projects/2024/KW 32/Financial Tracker/src/TrackerGUI.java
@@ -2,53 +2,106 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class TrackerGUI implements ActionListener {
-    TrackerGUI() {
+public class TrackerGUI {
+    Tracker t = new Tracker();
+    JTextField tf1, tf2, tf3, tf4;
+    JLabel lb, lb1, lb2, lb3, lb4;
+    JButton button1, button2, button3;
+
+    public TrackerGUI() {
         JFrame frame = new JFrame("Finance Tracker");
 
 
-        JTextField tf = new JTextField("Left: ");
-        tf.setBounds(315, 30, 35, 20);
-        tf.setEditable(false);
 
-        JTextField tf1 = new JTextField("Total Income: ");
-        tf1.setBounds(15, 30, 90, 20);
-        tf1.setEditable(false);
+        lb = new JLabel("Balance: " + t.getBalance());
+        lb.setBounds(315, 30, 150, 20);
 
-        JTextField tf2 = new JTextField("Total Expense: ");
-        tf2.setBounds(165, 30, 90, 20);
-        tf2.setEditable(false);
+        lb1 = new JLabel("Total Income: " + t.getTotalIncomesOrExpenses(t.incomes));
+        lb1.setBounds(15, 30, 200, 20);
 
-        JTextField tf3 = new JTextField();
-        tf3.setBounds(165, 150, 90, 20);
-        JButton button1 = new JButton("Add income");
+
+        lb2 = new JLabel("Total Expense: " + t.getTotalIncomesOrExpenses(t.expenses));
+        lb2.setBounds(165, 30, 200, 20);
+
+// todo... Label for name and amount of transaction
+        tf1 = new JTextField();
+        tf2 = new JTextField();
+        tf1.setBounds(165, 150, 90, 20);
+        tf2.setBounds(265, 150, 90, 20);
+        button1 = new JButton("Add income");
         button1.setBounds(15, 150, 110, 20);
 
-        JTextField tf4 = new JTextField();
-        tf4.setBounds(165, 175, 90, 20);
-        JButton button2 = new JButton("Add expense");
+        tf3 = new JTextField();
+        tf4 = new JTextField();
+        tf3.setBounds(165, 175, 90, 20);
+        tf4.setBounds(265, 175, 90, 20);
+        button2 = new JButton("Add expense");
         button2.setBounds(15, 175, 110, 20);
 
 
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = tf1.getText();
+                String valueText = tf2.getText();
+                try {
+                    double value = Double.parseDouble(valueText);
+                    t.addIncome(name, value);
 
-        button1.addActionListener(this);
+                    lb.setText("Balance: " + t.getBalance());
+                    lb1.setText("Total Income: " + t.getTotalIncomesOrExpenses(t.incomes));
+
+                    JOptionPane.showMessageDialog(frame, "Successfully added your income. New Balance: " + t.getBalance());
+
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a valid number for the value.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = tf3.getText();
+                String valueText = tf4.getText();
+                try {
+                    double value = Double.parseDouble(valueText);
+                    t.addExpense(name, value);
+
+                    lb.setText("Balance: " + t.getBalance());
+                    lb2.setText("Total Expense: " + t.getTotalIncomesOrExpenses(t.expenses));
+
+
+                    JOptionPane.showMessageDialog(frame, "Successfully added your expense. New Balance: " + t.getBalance());
+
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a valid number for the value.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        /*button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });*/
+
+
         frame.add(button1);
         frame.add(button2);
+        //frame.add(button3);
+        frame.add(lb);
+        frame.add(lb1);
+        frame.add(lb2);
         frame.add(tf1);
         frame.add(tf2);
         frame.add(tf3);
         frame.add(tf4);
-        frame.add(tf);
 
-
-        frame.setSize(400, 400);
+        frame.setSize(450, 400);
         frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
-    
 }
