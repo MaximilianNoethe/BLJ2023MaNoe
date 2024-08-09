c009b1c433ef66cdfb533ed81c133cc95c77e813
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-08-09 11:35:31 +0200

    Implement list to display all transaction

--- Java/Projects/2024/KW 32/Financial Tracker/src/DetailedGUI.java
+++ Java/Projects/2024/KW 32/Financial Tracker/src/DetailedGUI.java
@@ -1,22 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class DetailedGUI {
-    JFrame frame = new JFrame("Finance Tracker");
-    JLabel lb;
-    Tracker t = new Tracker();
+    private JFrame frame = new JFrame("Finance Tracker");
 
-    DetailedGUI() {
-        lb = new JLabel("TEST");
-        lb.setBounds(0, 0, 100, 50);
-        lb.setFont(new Font(null, Font.PLAIN, 25)); // IMPORTANT FOR TRACKERGUI CLASS
+    DetailedGUI(Tracker t) {
+        DefaultListModel<String> incomeListModel = new DefaultListModel<>();
+        for (Transaction transaction : t.getDetailedIncomes()) {
+            incomeListModel.addElement(transaction.toString());
+        }
+        JList<String> incomeList = new JList<>(incomeListModel);
+        incomeList.setBorder(BorderFactory.createTitledBorder("INCOMES"));
 
-        frame.add(lb);
+
+        DefaultListModel<String> expenseListModel = new DefaultListModel<>();
+        for (Transaction transaction : t.getDetailedExpenses()) {
+            expenseListModel.addElement(transaction.toString());
+        }
+        JList<String> expenseList = new JList<>(expenseListModel);
+        expenseList.setBorder(BorderFactory.createTitledBorder("EXPENSES"));
+
+        frame.add(new JScrollPane(incomeList));
+        frame.add(new JScrollPane(expenseList));
+
+        frame.setLayout(new GridLayout(2, 1));
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        frame.setSize(450, 400);
-        frame.setLayout(null);
+        frame.setSize(850, 800);
         frame.setVisible(true);
     }
 }
